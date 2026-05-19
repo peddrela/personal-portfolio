1b7757d0faea77238e36711412d046fbe19fe7a8
Pedro Guzzo <pedroguzzocs@gmail.com>
2026-05-19 16:42:08 -0300

    Fix: Configurando base path no astro.config para o GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Coloque o link principal do seu GitHub Pages aqui:
+  site: 'https://peddrela.github.io', 
+  
+  // Coloque o nome exato do seu repositório entre barras aqui:
+  base: '/personal-portfolio', 
+});
